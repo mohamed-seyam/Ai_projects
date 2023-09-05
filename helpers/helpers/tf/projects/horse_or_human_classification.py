55ff4ca15de0e9422248dd9be7f2021d8f11eadd
mohamed-seyam <61396368+mohamed-seyam@users.noreply.github.com>
2023-09-05 15:50:33 +0300

    Included the datagenerator classes in the generator file inside io operations

--- helpers/helpers/tf/projects/horse_or_human_classification.py
+++ helpers/helpers/tf/projects/horse_or_human_classification.py
@@ -5,32 +5,14 @@ from tensorflow.keras import layers
 from tensorflow.keras import Model
 from tensorflow.keras.optimizers import RMSprop
 
-def fetch_data():
-    # All images will be rescaled by 1./255
-    train_datagen = ImageDataGenerator(rescale=1/255)
-    validation_datagen = ImageDataGenerator(rescale=1/255)
+def fetch_data(train_dir, validation_dir):
+    from helpers.ai.io.generators import train_val_generators
+    train_gen, validation_gen = train_val_generators(training_dir=train_dir, validation_dir=validation_dir)
+    return train_gen, validation_gen
 
-    # Flow training images in batches of 128 using train_datagen generator
-    train_generator = train_datagen.flow_from_directory(
-            './data/tf/horse-or-human-data/training-horse-or-human/',  # This is the source directory for training images
-            target_size=(150, 150),  # All images will be resized to 150x150
-            batch_size=128,
-            # Since you used binary_crossentropy loss, you need binary labels
-            class_mode='binary')
-
-    # Flow training images in batches of 128 using train_datagen generator
-    validation_generator = validation_datagen.flow_from_directory(
-            './data/tf/horse-or-human-data/validation-horse-or-human/',  # This is the source directory for training images
-            target_size=(150, 150),  # All images will be resized to 150x150
-            batch_size=32,
-            # Since you used binary_crossentropy loss, you need binary labels
-            class_mode='binary')
-
-    return train_generator, validation_generator
 
 
 def train_horse_or_human_model(train_generator, validation_generator):
-
 
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(16, (3,3), activation = "relu", input_shape = (150,150,3)),
@@ -69,10 +51,10 @@ def train_horse_or_human_model(train_generator, validation_generator):
     return history
 
 
-def train_horse_or_human_model_using_transfer_learning():
+def train_horse_or_human_model_using_transfer_learning(train_gen, validation_gen):
     from tensorflow.keras.applications.inception_v3 import InceptionV3
     
-    local_weights_file = '/tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+    local_weights_file = r'data\models_data\inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
     
     pre_trained_model = InceptionV3(input_shape = (150, 150, 3),
                                   include_top = False, 
@@ -108,12 +90,33 @@ def train_horse_or_human_model_using_transfer_learning():
     model = Model(inputs= pre_trained_model.inputs, outputs= x)
 
     # Compile the model
-    model.compile(optimizer = RMSprop(learning_rate=0.0001), 
-                    loss = binary_crossentropy,
+    model.compile(optimizer = tf.keras.optimizers.RMSprop(lr = .001), 
+                    loss = tf.keras.losses.BinaryCrossentropy(),
                     metrics = ["accuracy"])
-def main():
-    train_gen, validation_gen = fetch_data()
-    train_horse_or_human_model(train_gen, validation_gen) 
+    
+    print(model.summary())
 
+    # Train the model
+    # Your model should achieve the desired accuracy in less than 15 epochs.
+    # You can hardcode up to 20 epochs in the function below but the callback should trigger before 15.
+    callback = TrackAccCallback()
+    history = model.fit(
+      train_gen,
+      steps_per_epoch=8,  
+      epochs=15,
+      verbose=1,
+      validation_data = validation_gen,
+      validation_steps=8, 
+      callbacks = [callback])
+    
+    ### END CODE HERE
+    return history
+def main():
+    training_dir = './data/tf/horse-or-human-data/training-horse-or-human/'
+    validation_dir = './data/tf/horse-or-human-data/validation-horse-or-human/'
+
+    train_gen, validation_gen = fetch_data(training_dir, validation_dir)
+    # train_horse_or_human_model(train_gen, validation_gen) 
+    history  = train_horse_or_human_model_using_transfer_learning(train_gen, validation_gen)
 if __name__ == "__main__":
     main()
