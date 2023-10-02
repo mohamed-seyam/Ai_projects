ff42096b25606960b83b87b2d6f0094a0fee08ac
mohamed-seyam <61396368+mohamed-seyam@users.noreply.github.com>
2023-10-02 09:39:49 +0300

    Finished Implementing siamese network for clothing similarity

--- helpers/helpers/tf/projects/clothing_similarity_with_siamise_network.py
+++ helpers/helpers/tf/projects/clothing_similarity_with_siamise_network.py
@@ -65,6 +65,16 @@ def prepare_data():
 
     return tr_pairs, tr_y, ts_pairs, ts_y
 
+def euclidean_distance(vects):
+    x, y = vects 
+    sum_squares = tf.keras.backend.sum(tf.keras.backend.square(x-y), axis=1, keepdims=True)
+    return tf.keras.backend.sqrt(tf.keras.backend.maximum(sum_squares, tf.keras.backend.epsilon()))
+
+
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
+
 def initialize_base_network():
     input = tf.keras.layers.Input(shape=(28,28,), name="base_input")
     x = tf.keras.layers.Flatten(name="flatten_input")(input)
@@ -75,6 +85,85 @@ def initialize_base_network():
     x = tf.keras.layers.Dense(128, activation='relu', name="third_base_dense")(x)
 
     return tf.keras.Model(inputs=input, outputs=x)
+
+
+def initialize_siamese_network(base_network, debug):
+    left_input = tf.keras.layers.Input(shape=(28,28,), name="left_input")
+    right_input = tf.keras.layers.Input(shape=(28,28,), name="right_input")
+
+    # siamese network
+    vector_output_a = base_network(left_input)
+    vector_output_b = base_network(right_input)
+
+    # measure the similarity of the two vector outputs
+    output = tf.keras.layers.Lambda(euclidean_distance, name="output_layer", output_shape=eucl_dist_output_shape)([vector_output_a, vector_output_b])
+
+    model = tf.keras.Model(inputs=[left_input, right_input], outputs=output)
+
+    if debug:
+        print(f"{Color.RED} Plotting the siamese network {Color.OFF}")
+        tf.keras.utils.plot_model(model, r"data\tmp\network_imgs\siamese_model.png")
+        print(f"{Color.RED} plot saved at {Color.OFF}  {Color.GREEN} data\\tmp\\network_imgs\ {Color.OFF} {Color.OFF}")
+
+    return model
+def contrastive_loss_with_margin(margin):
+    def contrastive_loss(y_true, y_pred):
+        '''Contrastive loss from Hadsell-et-al.'06
+        http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+        '''
+        square_pred = tf.keras.backend.square(y_pred)
+        margin_square = tf.keras.backend.square(tf.keras.backend.maximum(margin - y_pred, 0))
+        return tf.keras.backend.mean(y_true * square_pred + (1 - y_true) * margin_square)
+    return contrastive_loss
+
+
+def compute_accuracy(y_true, y_pred):
+    '''Compute classification accuracy with a fixed threshold on distances.
+    '''
+    pred = y_pred.ravel() < 0.5
+    return np.mean(pred == y_true)
+
+def plot_metrics(metric_name, title, ylim=5, history=None):
+    plt.title(title)
+    plt.ylim(0,ylim)
+    plt.plot(history.history[metric_name],color='blue',label=metric_name)
+    plt.plot(history.history['val_' + metric_name],color='green',label='val_' + metric_name)
+    plt.show()
+
+# Matplotlib config
+def visualize_images():
+    plt.rc('image', cmap='gray_r')
+    plt.rc('grid', linewidth=0)
+    plt.rc('xtick', top=False, bottom=False, labelsize='large')
+    plt.rc('ytick', left=False, right=False, labelsize='large')
+    plt.rc('axes', facecolor='F8F8F8', titlesize="large", edgecolor='white')
+    plt.rc('text', color='a8151a')
+    plt.rc('figure', facecolor='F0F0F0')# Matplotlib fonts
+
+
+# utility to display a row of digits with their predictions
+def display_images(left, right, predictions, labels, title, n):
+    plt.figure(figsize=(17,3))
+    plt.title(title)
+    plt.yticks([])
+    plt.xticks([])
+    plt.grid(None)
+    left = np.reshape(left, [n, 28, 28])
+    left = np.swapaxes(left, 0, 1)
+    left = np.reshape(left, [28, 28*n])
+    plt.imshow(left)
+    plt.figure(figsize=(17,3))
+    plt.yticks([])
+    plt.xticks([28*x+14 for x in range(n)], predictions)
+    for i,t in enumerate(plt.gca().xaxis.get_ticklabels()):
+        if predictions[i] > 0.5: t.set_color('red') # bad predictions in red
+    plt.grid(None)
+    right = np.reshape(right, [n, 28, 28])
+    right = np.swapaxes(right, 0, 1)
+    right = np.reshape(right, [28, 28*n])
+    plt.imshow(right)
+    plt.show()
+
 
 def main():
     debug = True
@@ -99,8 +188,31 @@ def main():
     base_network = initialize_base_network()
     if debug:
         print(f"{Color.RED} Plotting the base network {Color.OFF}")
-        tf.keras.utils.plot_model(base_network, r"data\tmp\network_imgs\siamese_model.png")
+        tf.keras.utils.plot_model(base_network, r"data\tmp\network_imgs\siamese_base_model.png")
         print(f"{Color.RED} plot saved at {Color.OFF}  {Color.GREEN} data\\tmp\\network_imgs\ {Color.OFF} {Color.OFF}")
 
+    siamese_model = initialize_siamese_network(base_network, debug)
+
+    rms = tf.keras.optimizers.RMSprop()
+    siamese_model.compile(loss=contrastive_loss_with_margin(margin=1), optimizer=rms)
+    history = siamese_model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
+                        batch_size=128,
+                        epochs=10,
+                        validation_data=([ts_pairs[:, 0], ts_pairs[:, 1]], ts_y))
+    
+    loss = siamese_model.evaluate(x=[ts_pairs[:,0],ts_pairs[:,1]], y=ts_y)
+
+    y_pred_train = siamese_model.predict([tr_pairs[:,0], tr_pairs[:,1]])
+    train_accuracy = compute_accuracy(tr_y, y_pred_train)
+
+    y_pred_test = siamese_model.predict([ts_pairs[:,0], ts_pairs[:,1]])
+    test_accuracy = compute_accuracy(ts_y, y_pred_test)
+
+    print("Loss = {}, Train Accuracy = {} Test Accuracy = {}".format(loss, train_accuracy, test_accuracy))
+    plot_metrics(metric_name='loss', title="Loss", ylim=0.2, history=history)
+
+    y_pred_train = np.squeeze(y_pred_train)
+    indexes = np.random.choice(len(y_pred_train), size=10)
+    display_images(tr_pairs[:, 0][indexes], tr_pairs[:, 1][indexes], y_pred_train[indexes], tr_y[indexes], "clothes and their dissimilarity", 10)
 if __name__ == "__main__":
     main()
